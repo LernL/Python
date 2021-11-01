9ffbac0d19c9d5079ff3ea133239e4fd1ef702f0
LernL <lernandlord@gmail.com>
2021-11-01 12:38:48 +0200

    Python

---
+++ Class/9.2.py
@@ -0,0 +1,12 @@
+#mashin=["BMW","Mersede","Lamborgini","Ilya"]
+#for i in mashin:
+#    print(i)
+#for i in range(0,3):
+#    print(mashin[i])
+#print(mashin[0:3])
+#print(' '.join(mashin))
+name = ["Yulia","Roma","Artem","Alexey","Tima"]
+a = name[:]
+a.insert(2,"Den")
+print (name)
+print(a)
