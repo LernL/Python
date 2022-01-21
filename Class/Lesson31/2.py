2eeeae058d9c34a9ba16edc9f61c6b94dc5881b4
LernL <lernandlord@gmail.com>
2022-01-21 15:59:02 +0200

    Pythom

---
+++ Class/Lesson31/2.py
@@ -0,0 +1,4 @@
+from User import *
+from Admin_and_Privelges import *
+a1=Admin("Tima","Pylypenko",16,"Ukraine",2015,["Allowed to add","Allowed to delete users","Allowed to ban users"])
+a1.show_privileges()
